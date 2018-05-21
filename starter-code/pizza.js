4359d609d0fed7e0e0647c602ad769836c6c6f73
Alex González <alexgm@MacBook-Pro-de-Alex.local>
2018-05-21 20:09:18 +0200

    jquery pizza

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,59 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function () {
+    clickButton();
+});
+
+function clickButton() {
+    $(".btn").click(function () {
+        $(this).toggleClass("active");
+        switch ($(this).text()) {
+            case "Pepperonni":
+                toggleItemsPizza(".btn-pepperonni",".pep",".price-pepperonni");
+                calculatePrice(".btn-pepperonni",1);
+                break;
+            case "Mushrooms":
+                toggleItemsPizza(".btn-mushrooms",".mushroom",".price-mushrooms");
+                calculatePrice(".btn-mushrooms",1);
+                break;
+            case "Green peppers":
+                toggleItemsPizza(".btn-green-peppers",".green-pepper",".price-peppers");
+                calculatePrice(".btn-green-peppers",1);
+                break;
+            case "White sauce":
+                toggleItemsPizza(".btn-sauce",".sauce",".price-sauce");
+                calculatePrice(".btn-sauce",3);
+                break;
+            case "Gluten-free crust":
+                $(".price-crust").toggle();
+                calculatePrice(".btn-crust",5);
+                break;
+        }
+        var $basePrice = $(".basePrice").text();
+        console.log($basePrice)
+
+    })
+}
+
+function toggleItemsPizza(classbutton,classNameItem,classNamePrice){ 
+    if ($(classbutton).hasClass("active")) {
+        $(classNameItem).toggle();
+        $(classNamePrice).toggle();
+        
+    } else {
+        $(classNameItem).toggle();
+        $(classNamePrice).toggle();
+        
+    }
+}
+
+function calculatePrice(classbutton,price){
+    var $total = Number($(".total-price").text());
+    if ($(classbutton).hasClass("active")) {
+        $total = $total + price
+    }else{
+        $total = $total - price
+    }
+    
+    $(".total-price").text($total);
+
+}
